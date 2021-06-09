4f7297d9c4fb6e7fd9fc783ab81b5ca9c36addf7
DavidCerchiaro <davidcerchiaro8@gmail.com>
2021-06-09 18:19:31 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';  //para cambiar de ventas con react
+import styled from 'styled-components';
+import { BrowserRouter, Route /*,Link*/ } from 'react-router-dom';  //para cambiar de ventas con react
+import Header from './Components/Header'
 import Inicio from './Components/Inicio';
 import Blog from './Components/Blog';
 import Acerca_de from './Components/Acerca_de';
 
-
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <h1>Mi blog</h1>
-          <nav>
-            <Link to="/">Inicio</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/acerca_de">Acerca de</Link>
-          </nav>
-        </header>
-        <main>
+      <ContenedorPrincipal>
+        <Header/>
+        <Main>
           <Route path="/" exact={true}>
             <Inicio />
           </Route>
@@ -27,10 +21,23 @@ function App() {
           <Route path="/acerca_de">
             <Acerca_de />
           </Route>
-        </main>
-      </div>
+        </Main>
+      </ContenedorPrincipal>
     </BrowserRouter >
   );
 }
+
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%;
+  max-width: 700px;
+`;
+const Main = styled.main`
+background-color: #B3B6B7;
+padding: 40px;
+border-radius: 10px;
+box-shadow: 0px 0px 5px rgba(129,129,129,.1);
+`;
+
 
 export default App;
